dac3fa613fd67cc6c505d7e79529fc811e69178f
Peter Reimer <peter@4pi.org>
2014-04-09 22:44:14 +0200

    doi validation: prefix allowed characters

--- dipp/datacite/datacite.py
+++ dipp/datacite/datacite.py
@@ -6,6 +6,8 @@ import urlparse
 import base64
 import codecs
 import logging
+import sys
+import string
 import os.path
 import ConfigParser
 from resources import DOI, METADATA
@@ -17,14 +19,16 @@ console = logging.StreamHandler()
 formatter = logging.Formatter('%(levelname)s - %(message)s')
 console.setFormatter(formatter)
 logger.addHandler(console)
+log_string = "%s API %s method: status %s"
 
 """https://mds.datacite.org/static/apidoc"""
 
 class Client:
     
-    def __init__(self, user, password, endpoint, testMode=False):
+    def __init__(self, user, password, prefix, endpoint, testMode=False):
         
         self.endpoint = endpoint
+        self.prefix = prefix
         self.testMode = testMode
         self.auth_string = base64.encodestring('%s:%s' % (user, password))
 
@@ -41,22 +45,49 @@ class Client:
         uri = urlparse.urlunparse(('https',self.endpoint,path,'',query,''))
         logger.info(uri)
         return uri
-   
+    
+    def validate_doi(self, doi):
+        """check if the doi contains only the recommended charakters
+        i'm pretty sure this could be done more elegant with reg exp...
+        """  
+        separator = '/'
+        allowed = list(string.ascii_lowercase) + ['-','.','_','+',':','/'] + [str(i) for i in range(0, 10)]
+        parts = doi.strip().lower().split(separator)
+        
+        validity = True
+        reason = ""
+        if len(parts) < 2:
+            validity = False
+            reason = "separator '%s' missing" % separator
+        elif parts[0] != self.prefix:
+            validity = False
+            reason = "wrong prefix, should be '%s'" % self.prefix
+        else:
+            for x in parts[1]:
+                if x not in allowed:
+                    validity = False
+                    reason="character '%s' not allowed" % x
+                    break
+        logger.info('%s valid: %s, %s' % (doi, validity, reason))
+        return validity
+
     def get_url(self, doi):
         """URI: https://test.datacite.org/mds/doi/{doi} where {doi} is a
         specific DOI.
         This GET request returns an URL associated with a given DOI.
         """
         h = httplib2.Http()
-        uri = self._make_rest_uri(DOI,doi=doi)
+        resource = DOI
         method = 'GET'
+        uri = self._make_rest_uri(resource, doi=doi)
         headers = {
             'Accept':'application/xml',
             'Authorization':'Basic ' + self.auth_string
             }   
         response, content = h.request(uri, method, headers=headers)
-        logger.info(response['status'])
-        return content
+        status = response['status']
+        logger.info(log_string % (resource, method, status))
+        return status, content
 
     def create_or_modify_doi(self, doi, url):
         """URI: https://test.datacite.org/mds/doi
@@ -67,8 +98,9 @@ class Client:
         will be increased by 1. A new record in Datasets will be created .
         """
         h = httplib2.Http()
-        uri = self._make_rest_uri(DOI,doi=doi)
+        resource = DOI
         method = 'PUT'
+        uri = self._make_rest_uri(resource,doi=doi)
         body_unicode = "doi=%s\n url=%s" % (doi, url)
         body = body_unicode.encode('utf-8')
         headers = {
@@ -78,7 +110,7 @@ class Client:
         
         response, content = h.request(uri, method, body=body, headers=headers)
         status = response['status']
-        logger.info(status)
+        logger.info(log_string % (resource, method, status))
         return status, content
     
     def post_metadata(self, metadata):
@@ -94,7 +126,7 @@ class Client:
             }   
         response, content = h.request(uri, method, body=body, headers=headers)
         status = response['status']
-        logger.info(status)
+        logger.info('metadata API - post, %s ' % status)
         return status, content
 
     def get_metadata(self, doi):
@@ -167,10 +199,33 @@ class Client:
 def main():
 
     parser = argparse.ArgumentParser(description='Manages DOIs at DataCite')
-    parser.add_argument('-d','--doi', help='registered DOI')
-    parser.add_argument('-c','--conf', help='Configuration file with access data')
-    parser.add_argument('-n','--dry-run', help='The request will not change the database nor will the DOI handle be registered or updated')
+    parser.add_argument('doi', help='DOI')
+    parser.add_argument('-u','--url', help='Url of article')
+    parser.add_argument('-c','--conf', required=True, help='Configuration file with access data')
+    parser.add_argument('-n','--dry-run', action='store_true', dest='testMode', help='The request will not change the database nor will the DOI handle be registered or updated')
     
+    args = parser.parse_args()
+    config_file = args.conf
+
+    if os.path.isfile(config_file):
+        config = ConfigParser.RawConfigParser()
+        config.read(config_file)
+        user = config.get('DataCite','user')
+        prefix = config.get('DataCite','prefix')
+        password = config.get('DataCite','password')
+        endpoint = config.get('DataCite','endpoint')
+    else:
+        print "%s does not exist" % config_file
+        sys.exit()
+
+    logger.info('user:   %s' % user)
+    logger.info('prefix: %s' % prefix)
+
+    client = Client(user, password, prefix, endpoint, testMode=args.testMode)
+    print client.get_url(args.doi)
+    print client.validate_doi(args.doi)
+    
+
 
 if __name__ == '__main__':
 
@@ -188,16 +243,35 @@ if __name__ == '__main__':
         config.read(config_file)
         user = config.get('DataCite','user')
         password = config.get('DataCite','password')
+        prefix = config.get('DataCite','prefix')
         endpoint = config.get('DataCite','endpoint')
     
-        x = Client(user, password, endpoint, testMode=test)
+        x = Client(user, password, prefix, endpoint, testMode=test)
         # print x.get_metadata(doi)
         
         # print x.create_or_modify_doi()
         #print x.post_metadata(md)
         #print x.create_or_modify_doi(doi, url)
         #print x.deactivate_doi(doi)
-        print x.get_metadata(doi)
+        print prefix
+        print doi, x.validate_doi(doi)
+        doi=""
+        print doi, x.validate_doi(doi)
+        
+        doi="10.5072/SDFas-sdf123"
+        print doi, x.validate_doi(doi)
+        
+        doi="10.5072sdfas-sdf"
+        print doi, x.validate_doi(doi)
+        
+        doi="10.5072/sdfas#sdf"
+        print doi, x.validate_doi(doi)
+        
+        doi="10.5073/sdfas#sdf"
+        print doi, x.validate_doi(doi)
+        
+        doi="10.5072/sdfas/sdf"
+        print doi, x.validate_doi(doi)
         
     else:
         print "%s does not exist" % config_file
